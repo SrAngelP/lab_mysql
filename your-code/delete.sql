667275249a04b30f10cd1deef971e5aa932f57ff
SrAngelP <114986879+SrAngelP@users.noreply.github.com>
2022-10-31 20:23:57 -0600

    Updated

---
+++ your-code/delete.sql
@@ -0,0 +1,16 @@
+
+USE ventacoches;
+
+SHOW TABLES;
+
+SELECT * FROM car;
+
+DELETE FROM car
+WHERE Model = 'V60';
+
+
+
+
+
+
+
---
+++ your-code/update.sql
@@ -0,0 +1,31 @@
+
+
+USE ventacoches;
+
+SHOW TABLES;
+
+SELECT * FROM customer;
+
+UPDATE salesperson
+SET tienda = 'Miami'
+WHERE id = 00004;
+
+UPDATE customer 
+SET email = 'ppicasso@gmail.com'
+WHERE id = 10001;
+
+UPDATE customer 
+SET email = 'lincoln@us.gov'
+WHERE id = 20001;
+
+UPDATE customer 
+SET email = 'hello@napoleon.me'
+WHERE id = 30001;
+
+
+
+
+
+
+
+
